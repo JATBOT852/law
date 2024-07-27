43dfae30d4ccb4eb4ea8dd1cd43998affcf6242a
JATBOT852 <jatbot36@gmail.com>
2024-07-27 18:10:17 +0530

    Update ollama.py

--- Backend/ollama.py
+++ Backend/ollama.py
@@ -55,4 +55,4 @@ def ask():
     return jsonify({'response': formatted_response,"question":questions})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0'port=8080)
+    app.run(host='0.0.0.0',port=8080)
